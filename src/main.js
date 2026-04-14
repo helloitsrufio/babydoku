b13c5451a699f7e601b11d97c071b28d10f8635f
helloitsrufio <ruthreed.dev@gmail.com>
2026-04-14 17:44:49 -0600

    feat: render generated loop

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 //TODO: we need to render this. currently it is not connected to any styles or our html whatsoever. maybe put something inside of childDivs?
 //grab #app and append parent div onto it
 let parentDiv = document.createElement("div");
-parentDiv.className("parent-grid");
-for (i = 0; i < 16; i++) {
+parentDiv.className = "parent-grid";
+
+for (let i = 0; i < 16; i++) {
   //make a child div
   let childDiv = document.createElement("div");
+  childDiv.innerHTML = i;
   // append child div to parent
   parentDiv.appendChild(childDiv);
-  childDiv.className("child-grid");
+  childDiv.className = "child-grid";
 }
 
-document.querySelector.appendChild("parentDiv");
+document.querySelector("#app").appendChild(parentDiv);
