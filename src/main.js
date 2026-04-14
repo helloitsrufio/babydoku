5950b4f178450c616a450c09d8d1d05bfcad2fc0
helloitsrufio <ruthreed.dev@gmail.com>
2026-04-14 16:42:10 -0600

    Add preliminary code

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// import "./style.css";
+//TODO: we need to render this. currently it is not connected to any styles or our html whatsoever. maybe put something inside of childDivs?
+//grab #app and append parent div onto it
+let parentDiv = document.createElement("div");
+parentDiv.className("parent-grid");
+for (i = 0; i < 16; i++) {
+  //make a child div
+  let childDiv = document.createElement("div");
+  // append child div to parent
+  parentDiv.appendChild(childDiv);
+  childDiv.className("child-grid");
+}
+
+document.querySelector.appendChild("parentDiv");
