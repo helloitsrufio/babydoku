cf10408ce52de41fa7fd1a2e3446412da6b55a48
helloitsrufio <ruthreed.dev@gmail.com>
2026-05-01 11:44:24 -0600

    wip: add initial gen loop

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 import "./style.css";
-//TODO: we need to render this. currently it is not connected to any styles or our html whatsoever. maybe put something inside of childDivs?
-//grab #app and append parent div onto it
 let parentDiv = document.createElement("div");
 parentDiv.className = "parent-grid";
 let row1 = [1, 4, 3, 2];
 let row2 = [3, 2, 1, 4];
 let row3 = [2, 1, 4, 3];
 let row4 = [4, 3, 2, 1];
-let gameboard = [row1, row2, row3, row4];
+let gameboard = [];
+
+let size = 4;
+
+//generate nums for gameboard
+function generateGameboard() {
+  for (let row = 0; row < size; row++) {
+    let row = [];
+    for (let column = 0; column < size; column++) {
+      row.push(Math.floor(Math.random() * size) + 1);
+      // if (gameboard[row] !== gameboard[row][column]) {
+      //   gameboard[row][column] = Math.random(min, max);
+    }
+    gameboard.push(row);
+  }
+}
+generateGameboard();
+console.log(gameboard);
+/**
+ * RULES FOR GRID NUMBER GENERATION
+ * populate squares with nums 1-4
+ * check the column at the same time to see if the column already has that num
+ * if it does, skip num
+ * if it does not, use num
+ * check if the current arr already has the num, if it already has, skip to next num
+ * num must be randomly generated between 1-4
+ */
+
+//RENDER LOOP
 for (let row = 0; row < 4; row++) {
   //the outer loop will run once, and when it does, it will run the inner loop 4 times, and THEN it will run its second iteration over again.
   //a mailman goes to the first floor and delivers mail to every apt, then goes to the next floor and does the same.
