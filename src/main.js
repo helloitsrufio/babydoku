4f20a9bd8ad5e6e943f4a100ba37812b8236d154
helloitsrufio <ruthreed.dev@gmail.com>
2026-06-09 19:46:16 -0600

    feat: add input to boxes in generated grid

--- src/main.js
+++ src/main.js
@@ -34,32 +34,49 @@ function generateGameboard() {
 }
 generateGameboard();
 console.log(gameboard);
-/**
- * RULES FOR GRID NUMBER GENERATION
- * populate squares with nums 1-4
- * check the column at the same time to see if the column already has that num
- * if it does, skip num
- * if it does not, use num
- * check if the current arr already has the num, if it already has, skip to next num
- * num must be randomly generated between 1-4
- */
 
-//RENDER LOOP
-for (let row = 0; row < 4; row++) {
-  //the outer loop will run once, and when it does, it will run the inner loop 4 times, and THEN it will run its second iteration over again.
-  //a mailman goes to the first floor and delivers mail to every apt, then goes to the next floor and does the same.
-  for (let col = 0; col < 4; col++) {
-    //make a child div
-    let cell = document.createElement("div");
-    cell.innerHTML = gameboard[row][col];
-    // append child div to parent
-    parentDiv.appendChild(cell);
-    cell.className = "cell-grid";
+// eventually we will pass a "solutionBoard" array
+function renderBoard() {
+  /**
+   * RULES FOR GRID NUMBER GENERATION
+   * populate squares with nums 1-4
+   * check the column at the same time to see if the column already has that num
+   * if it does, skip num
+   * if it does not, use num
+   * check if the current arr already has the num, if it already has, skip to next num
+   * num must be randomly generated between 1-4
+   */
+
+  //RENDER LOOP
+  for (let row = 0; row < 4; row++) {
+    //the outer loop will run once, and when it does, it will run the inner loop 4 times, and THEN it will run its second iteration over again.
+    //a mailman goes to the first floor and delivers mail to every apt, then goes to the next floor and does the same.
+    for (let col = 0; col < 4; col++) {
+      //make a child div
+      let cell = document.createElement("div");
+      let cellNum = gameboard[row][col];
+
+      // conditionally rendering based on a number
+      // using 4 as placeholder for 0
+      if (cellNum == 4) {
+        let numInput = document.createElement("input");
+        numInput.className = "cell-input";
+        numInput.inputMode = "numeric";
+        numInput.value = 0;
+        numInput.maxLength = 1;
+        parentDiv.appendChild(numInput);
+      } else {
+        cell.className = "cell-grid";
+        cell.innerHTML = gameboard[row][col];
+        parentDiv.appendChild(cell);
+      }
+    }
   }
+
+  document.querySelector("#app").appendChild(parentDiv);
 }
 
-document.querySelector("#app").appendChild(parentDiv);
-
+renderBoard();
 /**
  * what nums to show
  * what nums to hide
